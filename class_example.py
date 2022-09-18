a657dd9c841c909aa09bf9960f551486505bc8aa
Don Alejandro <AlexNgounou@GMail.com>
2022-09-18 17:39:25 -0400

    Code added in class

---
+++ class_example.py
@@ -0,0 +1,269 @@
+# number_one = 10
+# number_two = 87
+# number_two = number_one
+#
+# print(number_two)
+# what will be the value of number_one and number_two when we execute this code?
+
+#This language is Python!
+
+#Swift iOS = Objective-C
+
+#Brandon = none
+#Dorcas = 10 and 10
+#zion = 25/25
+#Ricky = none
+#Pat = 25/25
+#mutasim = 25/25
+#Justin = none OR 25/25
+#corine -= 25/25
+#Phoebe = 25/25
+#Patience = 25/25
+#
+# print("Hello World")
+# print(85)
+# print("developers will print logs to the console during code execution")
+
+
+
+
+## today 9/14/2022 we will work on print statements, input, as well as mathematical equations
+
+#Data Types
+#String = either characters or words. Strings need " " around them
+#Integers = whole numbers
+#Floats or Doubles = fractional numbers such as longitude and latitude
+
+# print("Hello World")
+# print("My name is Alex")
+# print("H")
+#
+# print("91422")
+# print(24 + 6)
+# print(234556655.56)
+#
+# first_name = "Alex"
+# last_name = "Ngounou"
+#
+# x = 12
+# y = 3
+# space = " "
+#
+# print(x + y)
+#
+# #in the line below, the print statement will concatenate first_name and last_name
+# print(first_name + space + last_name)
+#
+# print(first_name.upper())
+# print(first_name.lower())
+#
+#
+# print(x/y)
+# print(x*y)
+# print(x+y)
+# print(x-y)
+
+
+#Input
+game_first_name = input("What is your first name? ")
+game_last_name = input("What is your last name? ")
+game_age = input("How old are you? ")
+
+print("Your first name is " + game_first_name)
+print("Your last name is " + game_last_name)
+print("You are currently " + game_age + " years old!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
